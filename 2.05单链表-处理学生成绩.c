c560471994d90a91e52fe37394ae06b1c5120d8d
陈小雪 <67952140+chenxiaoxue315@users.noreply.github.com>
2020-08-24 00:26:16 +0800

    2.05单链表-处理学生成绩

---
+++ 2.05单链表-处理学生成绩.c
@@ -0,0 +1,64 @@
+/*链表处理学生成绩*/ 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stud_node {
+     int    num;
+     char   name[20];
+     int    score;
+     struct stud_node *next;
+};
+
+struct stud_node *createlist();
+struct stud_node *deletelist( struct stud_node *head, int min_score );
+
+int main()
+{
+    int min_score;
+    struct stud_node *p, *head = NULL;
+
+    head = createlist();
+    scanf("%d", &min_score);
+    head = deletelist(head, min_score);
+    for ( p = head; p != NULL; p = p->next )
+        printf("%d %s %d\n", p->num, p->name, p->score);
+
+    return 0;
+}
+struct stud_node *createlist(){
+	int number,score;
+	char name[100];
+	struct stud_node *p=NULL,*head=NULL,*tail=NULL;
+	scanf("%d",&number);
+	while(number!=0)
+	{
+		scanf("%s %d",name,&score);
+		p=(struct stud_node *)malloc(sizeof(struct stud_node));
+		p->num=number;
+		p->score=score;
+		strcpy(p->name,name);
+		p->next=NULL;
+		if(head==NULL) head=p;
+		else tail->next=p;
+		tail=p;
+		scanf("%d",&number);
+	 } 
+	 return head;
+} 
+struct stud_node *deletelist(struct stud_node *head,int min_score)
+{
+	struct stud_node *ptr=NULL,*ptr1=NULL,*ptr2=NULL;
+	if(head==NULL) return NULL;
+	for(ptr=head;ptr!=NULL;ptr=ptr->next)
+	{
+		if(ptr->score<min_score)
+		{
+			if(ptr==head) head=head->next;
+			else ptr1->next=ptr->next;
+			free(ptr1);
+		}
+		else
+		ptr1=ptr;
+	}
+	return head;
+}
